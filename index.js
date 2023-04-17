bab1fe353f4fbe4c3969c33ede2881e6c1234e23
Maral <maral.mamedovna@gmail.com>
2023-04-17 23:12:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names, surprise) {
+ let messages = [];
+ for (let i = 0; i < names.length; i++) {
+  let message = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`;
+  messages.push(message);
+ }
+ return messages;
+}
+
+let messages = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+console.log(messages);
+
+function countDown(num) {
+ let reverseCount = num;
+ while (reverseCount >= 0) {
+  console.log(reverseCount);
+  reverseCount--;
+ }
+ return reverseCount;
+}
+
+console.log(countDown(10));
+console.log(countDown(4));
+
